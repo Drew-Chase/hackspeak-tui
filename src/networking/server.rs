28a661bf99efa8fbf3117f17216a106047275a95
Drew Chase (Macbook) <dcmanproductions@gmail.com>
2026-04-21 17:38:02 -0400

    Add `User` struct to `Server` and include `connection_time` in `User` struct

--- src/networking/server.rs
+++ src/networking/server.rs
@@ -1,6 +1,7 @@
 use tokio::net::TcpStream;
+use crate::networking::user::User;
 
 pub struct Server{
 	stream: TcpStream,
-	
+	users: Vec<User>,
 }
--- src/networking/user.rs
+++ src/networking/user.rs
@@ -1,4 +1,6 @@
 pub struct User{
 	username: String,
 	ip: std::net::IpAddr,
+	connection_time: std::time::Instant,
+	
 }
